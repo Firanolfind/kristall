233b5f01bbb9716191f3f508d1454a1ed2d3f94d
Firanolfind <firanolfind@gmail.com>
2017-08-02 15:29:35 +0300

    config: sass globals by styleguide
    --HG--
    branch : dev

--- config/sass.js
+++ config/sass.js
@@ -11,9 +11,9 @@ module.exports = {
 	vars: (CONFIG, NODE_ENV) => {
 		var rel = path =>  Path.posix.relative(CONFIG.paths.build.styles.dir, path)
 		return {
-			$NODE_ENV:	NODE_ENV,
-			$PATH_IMG:	rel(CONFIG.paths.build.images.dir),
-			$PATH_FONT: rel(CONFIG.paths.build.fonts.dir)
+			$_NODE_ENV:		NODE_ENV,
+			$_PATH_IMG:		rel(CONFIG.paths.build.images.dir),
+			$_PATH_FONT: 	rel(CONFIG.paths.build.fonts.dir)
 		}
 	}
 };
