bf82efeb6ad3ae49f11743f9ce14017c06d83205
Firanolfind <firanolfind@gmail.com>
2017-09-18 19:24:17 +0300

    bin: command new scripts
    --HG--
    branch : feature

--- bin/index.js
+++ bin/index.js
@@ -6,6 +6,7 @@ const Util 	= require('util');
 const Yargs = require('yargs');
 const Path	= require('path');
 const FS	= require('fs');
+const FSE	= require('fs-extra')
 
 Yargs
 	.help(false)
@@ -23,6 +24,42 @@ Yargs
 		default: false,
 		description: 'verbose mode, shows processing details',
 		alias: 'V'
+	})
+	.option('appname', {
+		default: false,
+		description: 'application name',
+		alias: 'a',
+		type: 'string'
+	})
+	.option('title', {
+		default: false,
+		description: 'application title',
+		alias: 't',
+		type: 'string'
+	})
+	.option('username', {
+		default: false,
+		description: 'user name',
+		alias: 'u',
+		type: 'string'
+	})
+	.option('appversion', {
+		default: false,
+		description: 'application version',
+		alias: 'av',
+		type: 'string'
+	})
+	.option('desc', {
+		default: false,
+		description: 'user description',
+		alias: 'd',
+		type: 'string'
+	})
+	.option('license', {
+		default: false,
+		description: 'user license',
+		alias: 'l',
+		type: 'string'
 	})
 	.version('version', 'display version information', '1.0.1')
 	.alias('version', 'v')
@@ -47,22 +84,87 @@ const verbose = argv.verbose;
 
 
 
+
+
+// make dir
+const mkdir = dir => new Promise((resolve, reject)=>{
+	FS.mkdir(dir, err=>{
+		if(err){
+			reject(err);
+		}else{
+			resolve()
+		}
+	});
+});
+
+// make json
+const mkJson = (dir, fname, json)=> new Promise((resolve, reject)=>{
+	FS.writeFile(
+		Path.resolve(dir, fname),
+		JSON.stringify(json, null, "  "), err=>{
+			if(err){
+				reject(err);
+			}else{
+				resolve()
+			}
+		}
+	);
+});
+
+
 //Create new
 if(argv._.indexOf('new') >= 0){
-	console.log('instalation...');
-	console.log('instalation compleete');
 
-	var generatePackageJson = require('./package.json.js');
+	const dir = Path.posix.resolve(__dirname, '../', argv.path);
+	const generatePackageJson 	= require('./package.json.js');
 
-	var path = Path.posix.resolve(__dirname, '../', argv.path, 'package.json')
+	var defaultAppName = args0;
 
-	console.log(argv);
-	console.log(path);
+	// Ensure that default app name is not "." or "" and does not contain slashes.
+	if (defaultAppName === '.' || defaultAppName === './' || !defaultAppName) {
+		defaultAppName = path.basename(process.cwd());
+	}
+	defaultAppName = defaultAppName.replace(/\//, '-');
 
-	console.log(generatePackageJson({}));
+	var options = {
+		author: process.env.USER || 'anonymous node/sails user',
+		year: (new Date()).getFullYear(),
+		appName: defaultAppName
 
-	FS.writeFile(path, JSON.stringify(generatePackageJson({}), null, "  "), err=>{
-		console.error(err);
-	});
+	};
+
+
+
+	var package_json 			= generatePackageJson({});
+
+	mkdir(dir)
+		.then(()=>{
+			Promise.all([
+				mkJson(dir, 'package.json', package_json),
+				FSE.copy('app/config', Path.resolve(dir, 'config')),
+				FSE.copy('app/source', Path.resolve(dir, 'source')),
+				FSE.copy('app/tasks', Path.resolve(dir, 'tasks')),
+				FSE.copy('app/pipeline.js', Path.resolve(dir, 'pipeline.js')),
+				FSE.copy('app/server.js', Path.resolve(dir, 'server.js'))
+			]).catch(err=>{
+				console.error(err.red);
+				setImmediate(()=>{
+					process.exit(1);
+				});
+			})
+
+		})
+		.then(()=>{
+			console.log('done!'.cyan)
+		})
+		.catch(err=>{
+			console.log('dir already exists, cannot create project to existing directory'.red);
+			setImmediate(()=>{
+				process.exit(1);
+			});
+		})
+
+
+	
 
 }
